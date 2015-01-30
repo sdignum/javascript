09444731b9424630a192908f24a48ac23da37dc1
Shelley Dignum <shelleydignum@gmail.com>
2015-01-30 10:20:41 -0500

    added presidents objects and array

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var presidents = [{
+	firstName: "Barak" ,
+	lastName: "Obama" ,
+	termLength: "8 years",
+	party: "Democrat",
+	yearsOfPresidency: "2008-present" 
+},
+{
+	firstName: "George W." ,
+	lastName: "Bush" ,
+	termLength: "8 years",
+	party: "Republican",
+	yearsOfPresidency: "2000-2008"
+},
+{
+	firstName: "Bill" ,
+	lastName: "Clinton" ,
+	termLength: "8 years",
+	party: "Democrat",
+	yearsOfPresidency: "1992-2000"
+},
+{
+	firstName: "George H.W." ,
+	lastName: "Bush" ,
+	termLength: "4 years",
+	party: "Republican",
+	yearsOfPresidency: "1988-1992"
+},
+{
+	firstName: "Ronald" ,
+	lastName: "Reagan" ,
+	termLength: "8 years",
+	party: "Republican",
+	yearsOfPresidency: "1980-1988"
+}];
+
+console.log( presidents[2].firstName + " " + presidents[2].lastName );
